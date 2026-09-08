b41014f84c5689a41466ccfdba8b0a24c1d6ca24
Elia Renzoni <elia.renzoni03@gmail.com>
2026-09-08 08:46:14 +0200

    finish
    Signed-off-by: Elia Renzoni <elia.renzoni03@gmail.com>

--- internal/proto/resp_binder.go
+++ internal/proto/resp_binder.go
@@ -1,8 +1,16 @@
 package proto
 
+import "errors"
+
+var (
+	ErrBinderExpectedBstring = errors.New("Expected a bulk string")
+	ErrBinderExpectedCommand = errors.New("Expected a RESP Command")
+	ErrBinderArityNotEqual   = errors.New("Arity Not Equal")
+)
+
 type ExecutableCommand struct {
-	CommandType int
-	Args        []any
+	CommandType string
+	Args        []string
 }
 
 var cmdArity = map[int]int{
@@ -15,8 +23,31 @@ var cmdArity = map[int]int{
 func BindAST(ast ArrayAST) (ExecutableCommand, error) {
 	var cmd = ExecutableCommand{}
 
-	for _, val := range ast.Values {
+	arrLength := ast.ArrLength.Length.Literal
+	// read the Redis command
+	if arrLength == 0 {
+		// TODO-> define an error for this case
+		return ExecutableCommand{}, nil
 	}
 
-	return ExecutableCommand{}, nil
+	respCmd := ast.Values[0]
+	if respCmd.Bstring != nil {
+		return ExecutableCommand{}, ErrBinderExpectedBstring
+	}
+
+	arity, ok := cmdArity[respCmd.Bstring.Text.Token]
+	if !ok {
+		return ExecutableCommand{}, ErrBinderExpectedCommand
+	}
+
+	if arity != arrLength-1 {
+		return ExecutableCommand{}, ErrBinderArityNotEqual
+	}
+
+	cmd.CommandType = respCmd.Bstring.Text.Literal
+	for _, val := range ast.Values[1:] {
+		cmd.Args = append(cmd.Args, val.Bstring.Text.Literal)
+	}
+
+	return cmd, nil
 }
