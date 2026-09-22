869dd607111de4d8b447ed560892c390cf7fa64b
Elia Renzoni <elia.renzoni03@gmail.com>
2026-09-22 08:48:28 +0200

    feat: add virtual memory based memory management
    Signed-off-by: Elia Renzoni <elia.renzoni03@gmail.com>

---
+++ internal/functions/funcs.go
@@ -0,0 +1,40 @@
+package functions
+
+func Lpush() {
+}
+
+func Rpush() {
+}
+
+func Lpushx() {
+}
+
+func Lpop() {
+}
+
+func Rpop() {
+}
+
+func Blpop() {
+}
+
+func Llen() {
+}
+
+func Sadd() {
+}
+
+func Srem() {
+}
+
+func Smembers() {
+}
+
+func Sismember() {
+}
+
+func Scard() {
+}
+
+func Spop() {
+}
