eeee07fe0ce5a9cb0903d54a8739a93afcf29793
Elia Renzoni <elia.renzoni03@gmail.com>
2026-09-23 15:52:06 +0200

    fix
    Signed-off-by: Elia Renzoni <elia.renzoni03@gmail.com>

--- internal/functions/funcs.go
+++ internal/functions/funcs.go
@@ -2,25 +2,50 @@ package functions
 
 import "github.com/vector-hugo/internal/memory"
 
-func Lpush(bpool *memory.Arena) {
+func Lpush(bpool *memory.Arena, data []string) error {
+	for _, d := range data {
+		record := []byte(d)
+
+		err := bpool.Malloc(record, false)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
-func Rpush(bpool *memory.Arena) {
+func Rpush(bpool *memory.Arena, data []string) error {
+	for _, d := range data {
+		record := []byte(d)
+
+		err := bpool.Malloc(record, true)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
-func Lpushx(bpool *memory.Arena) {
+func Lpushx(bpool *memory.Arena, data []string) error {
+	return Lpush(bpool, data)
 }
 
-func Lpop(bpool *memory.Arena) {
+func Lpop(bpool *memory.Arena) error {
+	return bpool.Free(nil, false)
 }
 
-func Rpop(bpool *memory.Arena) {
+func Rpop(bpool *memory.Arena) error {
+	return bpool.Free(nil, true)
 }
 
 func Blpop(bpool *memory.Arena) {
 }
 
-func Llen(bpool *memory.Arena) {
+func Llen(bpool *memory.Arena) (int, error) {
+	_, size, err := bpool.Scan(nil)
+	return size, err
 }
 
 func Sadd(bpool *memory.Arena) {
